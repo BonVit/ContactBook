41660a6e7b66a42fd883866b48591f32cf708394
bonvit <bonarv96@gmail.com>
2017-06-13 19:53:09 +0300

    Add methods

--- app/src/main/java/com/vb/contactbook/mvp/model/database/IDatabase.java
+++ app/src/main/java/com/vb/contactbook/mvp/model/database/IDatabase.java
@@ -1,8 +1,14 @@
 package com.vb.contactbook.mvp.model.database;
 
+import com.vb.contactbook.mvp.model.entity.Contact;
+import com.vb.contactbook.mvp.model.entity.User;
+
 /**
  * Created by bonar on 6/13/2017.
  */
 
 public interface IDatabase {
+    void insert(User user);
+    void insert(Contact contact);
+    User getUserByGoogleId(String id);
 }
