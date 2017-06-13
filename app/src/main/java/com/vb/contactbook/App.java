eddb80cd9621eda1461f60ac51a9c2c1dfd3cfaa
bonvit <bonarv96@gmail.com>
2017-06-13 20:02:38 +0300

    Add appcomponent

--- app/src/main/java/com/vb/contactbook/App.java
+++ app/src/main/java/com/vb/contactbook/App.java
@@ -2,6 +2,11 @@ package com.vb.contactbook;
 
 import android.app.Application;
 
+import com.vb.contactbook.di.component.AppComponent;
+import com.vb.contactbook.di.component.DaggerAppComponent;
+import com.vb.contactbook.di.module.AppModule;
+import com.vb.contactbook.di.module.DatabaseModule;
+
 /**
  * Created by bonar on 6/13/2017.
  */
@@ -9,9 +14,19 @@ import android.app.Application;
 public class App extends Application {
     private static final String TAG = "App";
 
+    private static AppComponent mAppComponent;
+
+    public static AppComponent getAppComponent() {
+        return mAppComponent;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
 
+        mAppComponent = DaggerAppComponent.builder()
+                .appModule(new AppModule(this))
+                .databaseModule(new DatabaseModule())
+                .build();
     }
 }
