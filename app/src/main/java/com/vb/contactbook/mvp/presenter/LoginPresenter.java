30b73f7e6f0443bed1d867e580a223b2607797e3
bonvit <bonarv96@gmail.com>
2017-06-13 17:24:25 +0300

    Login system

--- app/src/main/java/com/vb/contactbook/mvp/presenter/LoginPresenter.java
+++ app/src/main/java/com/vb/contactbook/mvp/presenter/LoginPresenter.java
@@ -1,7 +1,17 @@
 package com.vb.contactbook.mvp.presenter;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.vb.contactbook.mvp.view.LoginView;
 
 /**
@@ -9,5 +19,66 @@ import com.vb.contactbook.mvp.view.LoginView;
  */
 
 @InjectViewState
-public class LoginPresenter extends MvpPresenter<LoginView> {
+public class LoginPresenter extends MvpPresenter<LoginView> implements GoogleApiClient.OnConnectionFailedListener {
+    private static final String TAG = "LoginPresenter";
+
+    private static final int GOOGLE_LOGIN_REQUEST_CODE = 1;
+
+    private GoogleSignInOptions mGoogleSignInOptions;
+    private GoogleApiClient mGoogleApiClient;
+
+    private FragmentActivity mFragmentActivity;
+
+    public LoginPresenter() {
+        Log.d(TAG, "constructor");
+    }
+
+    public void setup(FragmentActivity fragmentActivity) {
+        mFragmentActivity = fragmentActivity;
+        initGoogleClient();
+    }
+
+    private void initGoogleClient() {
+        mGoogleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestProfile()
+                .requestId()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(mFragmentActivity)
+                .enableAutoManage(mFragmentActivity, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, mGoogleSignInOptions)
+                .build();
+    }
+
+    public void signInWithGoogle() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        mFragmentActivity.
+                startActivityForResult(signInIntent, GOOGLE_LOGIN_REQUEST_CODE);
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        getViewState().googleSignIn(false);
+    }
+
+    public void handleActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case GOOGLE_LOGIN_REQUEST_CODE:
+                GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                handleGoogleSignInResult(googleSignInResult);
+                break;
+        }
+    }
+
+    private void handleGoogleSignInResult(GoogleSignInResult googleSignInResult) {
+        if(googleSignInResult.isSuccess()) {
+            //Logged in
+            getViewState().googleSignIn(true);
+        } else {
+            //Logged out
+            getViewState().googleSignIn(false);
+        }
+    }
 }
--- app/src/main/java/com/vb/contactbook/ui/LoginActivity.java
+++ app/src/main/java/com/vb/contactbook/ui/LoginActivity.java
@@ -1,28 +1,74 @@
 package com.vb.contactbook.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
+import com.google.android.gms.common.SignInButton;
 import com.vb.contactbook.R;
 import com.vb.contactbook.mvp.presenter.LoginPresenter;
 import com.vb.contactbook.mvp.view.LoginView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by bonar on 6/13/2017.
  */
 
-public class LoginActivity extends MvpAppCompatActivity implements LoginView {
+public class LoginActivity extends MvpAppCompatActivity implements LoginView,
+        View.OnClickListener {
     private static final String TAG = "LoginActivity";
 
     /*Moxy mvp binding*/
     @InjectPresenter
     LoginPresenter mLoginPresenter;
 
+    /*ButterKnife view binding*/
+    @BindView(R.id.googleSignIn)
+    SignInButton mGoogleSignInButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mLoginPresenter.setup(this);
+
+        mGoogleSignInButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.googleSignIn:
+                mLoginPresenter.signInWithGoogle();
+                break;
+        }
+    }
+
+
+    @Override
+    public void googleSignIn(boolean success) {
+        if(success) {
+            Toast.makeText(this, "Success", Toast.LENGTH_SHORT)
+                    .show();
+        } else {
+            Toast.makeText(this, getResources().getString(R.string.google_sign_in_failed), Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        mLoginPresenter.handleActivityResult(requestCode, resultCode, data);
     }
 }
