2dda8561f6dca67ae411ccb12ffdba00141f9b97
bonvit <bonarv96@gmail.com>
2017-06-13 15:51:33 +0300

    Add login mvp

---
+++ app/src/main/java/com/vb/contactbook/mvp/presenter/LoginPresenter.java
@@ -0,0 +1,13 @@
+package com.vb.contactbook.mvp.presenter;
+
+import com.arellomobile.mvp.InjectViewState;
+import com.arellomobile.mvp.MvpPresenter;
+import com.vb.contactbook.mvp.view.LoginView;
+
+/**
+ * Created by bonar on 6/13/2017.
+ */
+
+@InjectViewState
+public class LoginPresenter extends MvpPresenter<LoginView> {
+}
