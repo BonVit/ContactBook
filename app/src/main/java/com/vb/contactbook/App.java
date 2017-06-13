75a3b2b092ebf836fba5ee6146e9a0a1e66bbfa3
bonvit <bonarv96@gmail.com>
2017-06-13 17:22:07 +0300

    Add App

---
+++ app/src/main/java/com/vb/contactbook/App.java
@@ -0,0 +1,17 @@
+package com.vb.contactbook;
+
+import android.app.Application;
+
+/**
+ * Created by bonar on 6/13/2017.
+ */
+
+public class App extends Application {
+    private static final String TAG = "App";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
