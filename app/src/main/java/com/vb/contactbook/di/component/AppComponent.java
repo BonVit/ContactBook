bc61000320ed0389991a633508552576f70b87c6
bonvit <bonarv96@gmail.com>
2017-06-13 19:58:41 +0300

    Add Database module

--- app/src/main/java/com/vb/contactbook/di/component/AppComponent.java
+++ app/src/main/java/com/vb/contactbook/di/component/AppComponent.java
@@ -1,6 +1,7 @@
 package com.vb.contactbook.di.component;
 
 import com.vb.contactbook.di.module.AppModule;
+import com.vb.contactbook.di.module.DatabaseModule;
 import com.vb.contactbook.mvp.presenter.MainPresenter;
 
 import dagger.Component;
@@ -9,7 +10,7 @@ import dagger.Component;
  * Created by bonar on 6/13/2017.
  */
 
-@Component(modules = {AppModule.class})
+@Component(modules = {AppModule.class, DatabaseModule.class})
 public interface AppComponent {
     void inject(MainPresenter mainPresenter);
 }
